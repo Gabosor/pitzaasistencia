9dd3e5a3c3b2d15ac78a636ec5e9cb30d58ef0b0
Gabosor <z.kdeqth1@gmail.com>
2024-07-09 12:08:46 -0400

    Mas errores.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AttendanceController;
 
-Route::get('/asistencia', function () {
+Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
